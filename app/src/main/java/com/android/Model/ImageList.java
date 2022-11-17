7eb66abe82cdfb78825f27a06e6bdff340963393
AndroStuff <androstuff007@gmail.com>
2022-11-18 02:24:36 +0530

    images being fetched

---
+++ app/src/main/java/com/android/Model/ImageList.java
@@ -0,0 +1,17 @@
+package com.android.Model;
+
+public class ImageList {
+
+    private final String path;
+
+
+    public ImageList(String path) {
+        this.path = path;
+
+    }
+
+    public String getImagePath() {
+        return path;
+    }
+
+}
